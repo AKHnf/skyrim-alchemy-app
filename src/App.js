b555e989d95ac2ae10d7f86713b1736eef95b255
akhnf <haneefx7@gmail.com>
2020-01-07 14:47:31 +0800

    Change app.js to display hello world

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const name = 'Lieza';
+
+class App extends Component {
+  render() {
+    return (
+      <div class="container">
+        <h1>Hello, {name}!</h1>
+        <p>I just created my first React app</p>
+      </div>
+    )
+  }
 }
 
 export default App;
